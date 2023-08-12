fe40f78023c1f6b0385259be272e6be78d89e076
Edidiong Obodom <edijay17@gmail.com>
2023-08-13 00:24:20 +0100

    modified:   db.js

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 require("dotenv").config();
 const Pool = require("pg").Pool;
 
-const pool = new Pool({
-  user: "postgres",
-  password: process.env.DB_PASSWORD,
-  host: "localhost",
-  port: 5432,
-  database: "nunsaunical",
-});
-
 // const pool = new Pool({
-//   user: "myapp_4hvr_user",
-//   // user: "postgres",
+//   user: "postgres",
 //   password: process.env.DB_PASSWORD,
-//   host: "dpg-ci30ahrhp8u1a1d74bog-a",
-//   // host: "dpg-ci30ahrhp8u1a1d74bog-a.oregon-postgres.render.com",
+//   host: "localhost",
 //   port: 5432,
-//   database: "myapp_4hvr",
-//   // database: "reventlify",
-//   // ssl: true,
+//   database: "nunsaunical",
 // });
+
+const pool = new Pool({
+  user: "myapp_4hvr_user",
+  // user: "postgres",
+  password: process.env.DB_PASSWORD,
+  host: "dpg-ci30ahrhp8u1a1d74bog-a",
+  // host: "dpg-ci30ahrhp8u1a1d74bog-a.oregon-postgres.render.com",
+  port: 5432,
+  database: "nunsaunicaldev",
+  // database: "reventlify",
+  // ssl: true,
+});
 module.exports = pool;
