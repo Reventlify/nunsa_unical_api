bfb282574746951fefb3523973d00e73511ff1c3
Edidiong Obodom <edijay17@gmail.com>
2023-09-02 16:45:11 +0100

    modified:   controllers/user/services/regular_students/normal/searchFor/searchForStudents/searchForStudents.js

--- controllers/user/services/regular_students/normal/searchFor/searchForStudents/searchForStudents.js
+++ controllers/user/services/regular_students/normal/searchFor/searchForStudents/searchForStudents.js
@@ -6,10 +6,10 @@ exports.searchForStudents = async (req, res) => {
     const query = `
     SELECT student_id, student_fname, student_lname, student_photo FROM students WHERE 
     student_fname ILIKE $1 OR
-	student_lname ILIKE $1 OR
+	  student_lname ILIKE $1 OR
     (student_fname || ' ' || student_mname) ILIKE $1 OR
     (student_fname || ' ' || student_lname) ILIKE $1 OR
-	(student_lname || ' ' || student_fname) ILIKE $1 OR
+	  (student_lname || ' ' || student_fname) ILIKE $1 OR
     (student_fname || ' ' || student_mname || ' ' || student_lname) ILIKE $1 OR
     (student_lname || ' ' || student_mname || ' ' || student_fname) ILIKE $1 OR
     student_mat_no ILIKE $1
@@ -22,9 +22,9 @@ exports.searchForStudents = async (req, res) => {
       return res.status(404).json("Student not found");
     }
 
-    res.status(200).json(rows);
+    return res.status(200).json(rows);
   } catch (error) {
     console.error("Error executing database query:", error);
-    res.status(500).json("Something went wrong");
+    return res.status(500).json("Something went wrong");
   }
 };
