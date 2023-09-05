0d825747cf4d2705283b85a1e9f5b40079b0f146
Edidiong Obodom <edijay17@gmail.com>
2023-09-05 08:48:37 +0100

    modified:   controllers/user/services/regular_students/normal/messages/getMessages.js

--- controllers/user/services/regular_students/normal/messages/getMessages.js
+++ controllers/user/services/regular_students/normal/messages/getMessages.js
@@ -38,6 +38,53 @@ exports.getMessages = async (req, res) => {
 
     // Define the SQL query to get conversation
     const query = `
+      SELECT 
+      messages.message_id,
+      messages.conversation_id,
+      messages.sender_id,
+      messages.message_text,
+      messages.message_media,
+      messages.sent_at
+      FROM messages
+      LEFT JOIN conversations
+      ON
+      messages.conversation_id = conversations.conversation_id
+      WHERE 
+      conversations.user1_id = $1 AND conversations.user2_id = $2 
+      OR 
+      conversations.user1_id = $2 AND conversations.user2_id = $1
+      GROUP BY
+      messages.message_id,
+      messages.conversation_id,
+      messages.sender_id,
+      messages.message_text,
+      messages.message_media,
+      messages.sent_at
+      ORDER BY (messages.sent_at) ASC
+          `;
+
+    // Execute the SQL query with the search criteria
+    const { rows } = await pool.query(query, [sender, receiver]);
+
+    // if (rows.length === 0) {
+    //   return res.status(404).json("No messages in this chat");
+    // }
+
+    return res.status(200).json({
+      msg: rows,
+      partner: participant.rows[0],
+    });
+  } catch (error) {
+    console.error("Error executing database query:", error);
+    return res.status(500).json("Something went wrong");
+  }
+};
+
+exports.getConversations = async (req, res) => {
+  try {
+    const user = req.user;
+    // Define the SQL query to get conversation
+    const query = `
     WITH RankedMessages AS (
       SELECT
         m.conversation_id,
@@ -94,65 +141,8 @@ exports.getMessages = async (req, res) => {
         (rm.user2 = $1 AND rm.sender_id = rm.user1)
       )
     WHERE
-      rm.row_num = 1;    
+      rm.row_num = 1  
 `;
-    //     // Define the SQL query to get conversation
-    //     const query = `
-    //     WITH RankedMessages AS (
-    //       SELECT
-    //         m.conversation_id,
-    //         m.sender_id,
-    //         m.message_text,
-    //         m.message_media,
-    //         m.message_media_id,
-    //         m.delete_message,
-    //         m.seen,
-    //         m.sent_at,
-    //         ROW_NUMBER() OVER (PARTITION BY m.conversation_id ORDER BY m.sent_at DESC) AS row_num,
-    //         c.user1_id AS user1,
-    //         c.user2_id AS user2
-    //       FROM
-    //         messages m
-    //         INNER JOIN conversations c ON m.conversation_id = c.conversation_id
-    //       WHERE
-    //         c.user1_id = $1 OR c.user2_id = $1
-    //     )
-    //     SELECT DISTINCT ON (rm.conversation_id)
-    //       rm.conversation_id,
-    //       rm.sender_id,
-    //       rm.message_text,
-    //       rm.message_media,
-    //       rm.message_media_id,
-    //       rm.delete_message,
-    //       rm.seen,
-    //       rm.sent_at,
-    //       CASE
-    //         WHEN rm.sender_id = $1 THEN other_user.student_fname
-    //         ELSE sender_user.student_fname
-    //       END AS other_user_fname,
-    //       CASE
-    //         WHEN rm.sender_id = $1 THEN other_user.student_lname
-    //         ELSE sender_user.student_lname
-    //       END AS other_user_lname,
-    //       CASE
-    //         WHEN rm.sender_id = $1 THEN
-    //           CASE
-    //             WHEN rm.user1 = $1 THEN rm.user2
-    //             ELSE rm.user1
-    //           END
-    //         ELSE
-    //           CASE
-    //             WHEN rm.user1 = $1 THEN rm.user1
-    //             ELSE rm.user2
-    //           END
-    //       END AS other_user_id
-    //     FROM
-    //       RankedMessages rm
-    //       LEFT JOIN students sender_user ON rm.sender_id = sender_user.student_id
-    //       LEFT JOIN students other_user ON (rm.user1 = other_user.student_id OR rm.user2 = other_user.student_id)
-    //     WHERE
-    //       rm.row_num = 1
-    // `;
 
     // Execute the SQL query with the search criteria
     const { rows } = await pool.query(query, [user]);
