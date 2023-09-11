4fab3bc760df6442574f90ccf0ad3f4134bc40c8
Edidiong Obodom <edijay17@gmail.com>
2023-09-11 09:52:58 +0100

    modified:   controllers/user/services/excos/dues/viewStudent.js

--- controllers/user/services/excos/dues/viewStudent.js
+++ controllers/user/services/excos/dues/viewStudent.js
@@ -11,7 +11,7 @@ exports.viewStudent = async (req, res) => {
 
     if (studentRole.rows.length === 0) return res.status(400).json("Not found");
 
-    if (studentRole.rows[0].student_role !== "member")
+    if (studentRole.rows[0].student_role === "member")
       return res.status(400).json("Bad guy");
 
     const query = `
