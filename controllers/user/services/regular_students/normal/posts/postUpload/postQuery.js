e23645dadbae4dd8f09ebc9d220a38e41b6ef2ee
Edidiong Obodom <edijay17@gmail.com>
2023-09-14 14:08:10 +0100

    modified:   controllers/user/services/regular_students/normal/posts/postUpload/postQuery.js

--- controllers/user/services/regular_students/normal/posts/postUpload/postQuery.js
+++ controllers/user/services/regular_students/normal/posts/postUpload/postQuery.js
@@ -57,7 +57,7 @@ exports.postQuery = async (req, res) => {
         LEFT JOIN post_likes pl ON p.post_id = pl.post_id AND pl.student_id = $1
         LEFT JOIN post_dislikes pd ON p.post_id = pd.post_id AND pd.student_id = $1
         LEFT JOIN post_comments pc ON p.post_id = pc.post_id AND pc.student_id = $1
-        WHERE sch_session = $2 OR sch_session = $3
+        WHERE (sch_session = $3 OR sch_session = $4 AND $2 IS NOT NULL) OR (sch_session IS NULL AND $2 IS NULL)
       ORDER BY p.post_date DESC
       OFFSET 0
       LIMIT 20
@@ -139,7 +139,7 @@ exports.postQuery = async (req, res) => {
     // const { rows } = await pool.query(query, [helper]);
     const data =
       level === "200"
-        ? await pool.query(query, [student_id, "21/22", "20/21"])
+        ? await pool.query(query, [student_id, helper, "21/22", "20/21"])
         : await pool.query(query, [student_id, helper]);
     return res.status(200).json(data.rows);
   } catch (error) {
